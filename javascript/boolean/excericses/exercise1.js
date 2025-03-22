fc930fef2552c1a12c2defd8d8af1785f11a3fab
Yen Ka <yen@pulsedigital.co.uk>
2025-03-22 17:46:45 +0700

    [Javascript]: Exercise 1 and 2

---
+++ javascript/boolean/excericses/exercise1.js
@@ -0,0 +1,27 @@
+/**
+ * Viết hàm lấy giá vé tương ứng theo số tuổi
+  Khi khách hàng vào tham quan khu du lịch, tuỳ thuộc vào độ tuổi mà có giá vé khác nhau.
+  Hãy viết một hàm nhận vào số tuổi của khách hàng, trả về giá vé tương ứng.
+
+  Bảng giá như sau:
+
+  Dưới 6 tuổi hoặc từ 70 tuổi trở lên là được FREE (trả về 0)
+
+  Từ 6 - 12 tuổi giá vé là 20.000 VND
+
+  Trên 12 tuổi thì đồng giá 50.000 VND
+
+
+
+  Viết hàm function getTicketPrice(age)  để giúp mình lấy được giá tiền khi biết tuổi của khách hàng nhé.
+
+  Trả về là một con số nguyên:
+
+  -1 nếu dữ liệu không hợp lệ
+
+  0 nếu free
+
+  các giá tiền nếu tuổi hợp lệ
+
+  Giả sử người sống thọ nhất là 125 tuổi.
+ */
---
+++ javascript/boolean/excericses/exercise2.js
@@ -0,0 +1,18 @@
+/**
+ * Say hello bằng nhiều ngôn ngữ
+  Viết hàm sayHello(languageCode) nhận vào languageCode và trả về câu xin chào của ngôn ngữ tương ứng.
+
+  languageCode = 'en' --> Hello
+
+  languageCode = 'vi' --> Xin chào
+
+  languageCode = 'fr' --> Bonjour
+
+  languageCode = 'cn' --> Nǐn hǎo
+
+  languageCode = 'ja' --> Konnichiwa
+
+  languageCode = 'ko' --> Anyoung haseyo
+
+  Trường hợp không truyền languageCode hoặc languageCode không hợp lệ thì trả về mặc định "Hello"
+ */
