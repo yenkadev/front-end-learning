73ce5ae22ce7fa7888f455588ab68fc8e50c7104
Yen Ka <yen@pulsedigital.co.uk>
2025-03-29 15:16:58 +0700

    [Javascript]: Update Javascript overview doc

--- javascript/data-types.js
+++ javascript/data-types.js
@@ -1,16 +1,26 @@
 /**
- * Kiểu dữ liệu
- * Prirmitive: number, boolean, string, null, undefined, symbol
- * Non-Primitive: object, array
+ * Các kiểu dữ liệu trong Javascript
+ * - number: int hoặc float, integers được giới hạn ±(2^53-1)
+ * - bigint: số nguyên có độ dài tùy ý
+ * - string: chuỗi
+ * - boolean: true / false
+ * - null: giá trị không xác định (unknown values)
+ * - undefined: giá trị chưa được gán
+ * - symbol: unique identifiers
+ * - object: NON-Primitive type, được sử dụng cho cấu trúc dữ liệu phức tạp
  */
 
-const age = 18;
+const age = 28;
 const name = "Yen";
 const isActive = true;
 const student = {
   name: "Dong",
   age: 22
 } 
-let count; // Khai báo bằng const phải có giá trị khởi tạo
+let count;
+const university; // Khai báo bằng const phải gán giá trị khởi tạo
 
+/**
+ * Read more: https://javascript.info/types
+ */
 
--- javascript/variables.js
+++ javascript/variables.js
@@ -1,38 +1,49 @@
 /**
- * what: Lưu trữ giá trị lên bộ nhớ
- * why: Khống có cách khác để lưu tạm giá trị
- * when: Cần lưu giá trị
- * who: Dev
+ * Variables in Javascript
+ * - what: Lưu trữ giá trị lên bộ nhớ
+ * - why: Khống có cách khác để lưu tạm giá trị
+ * - when: Cần lưu giá trị
+ * - who: Dev
  */
 
 /**
  * Cách khai báo biến trong Js
- * ES5: Dùng var (function scope)
- * ES6: Dùng let và const (block scope)
- * Scope thì hiểu sau
- * Const thì dùng cho hằng số mà kg nhu cầu thay đổi 
- * Let thì dùng cho biến có mong muốn thay đổi giá trị
- * Tips: Luôn luôn dùng const cho đến khi cần let
+ * - ES5: Dùng var (function scope)
+ * - ES6: Dùng const hoặc let (block scope)
+ * - Scope thì hiểu sau
+ * - Const thì dùng cho hằng số hoặc biến mà kg nhu cầu thay đổi 
+ * - Let thì dùng cho biến có mong muốn thay đổi giá trị
+ * - Tips: Luôn luôn dùng const cho đến khi cần let
  */
+
+// ES5
+var name = 'Javascript course';
+
+// ES6
+const PI = 3.14;
+const name = 'Js';
+name = 'Course'; // error assignment to constant variable
+let count = 1;
+count = 2; // works
 
 /**
  * Cách đặt tên biến
- * Kg đặt tên bằn số, ký tự đặc biệt, dấu cách
- * Dùng danh từ
- * Đặt tên đúng nghĩa giữa giá trị và tên biến
- * Dùng tiếng anh
- * Với true/false thì đặt tên dùng prefix has hoặc is hoặc show hoặc từ mang ý nghĩa chỉ nhận giá trị true hoặc false
- * Với dạng danh sách thì suffix List
+ * - Kg đặt tên bằng số, ký tự đặc biệt, dấu cách
+ * - Đặt tên đúng nghĩa với giá trị được gán cho biến
+ * - Dùng danh từ để đặt tên biến
+ * - Dùng tiếng anh
+ * - Với true/false thì đặt tên dùng prefix has hoặc is hoặc show hoặc từ mang ý nghĩa chỉ nhận giá trị true hoặc false
+ * - Với dạng danh sách thì suffix List
  */
 
 const product = 'Js course'; // BAD
 const productName = 'Js course'; // GOOD
 
-// true/false varibales
+// true/false variable
 const hasValidItem = true;
+const hasPositiveNumber = false;
 const isSelected = true;
 const show = false;
-const hasPositiveNumber = false;
 
 // single noun
 const student = {
@@ -43,10 +54,10 @@ const student = {
 const studentList = [].map(student => console.log(student.name));
 
 /**
- * Kiểu đặt tên biến
- * camelCase: studentList (identifier)
- * UPPER_CASE: PI, API_URL (Constants)
- * PascalCase: Menu, Sidebar (Component or Class)
- * snake_case: student_name (unpopular Usage)
- * kebab-case: menu-item (Css class name)
+ * Case styles
+ * - camelCase: studentList (identifier)
+ * - UPPER_CASE: PI, API_URL (Constants)
+ * - PascalCase: Menu, Sidebar (Component or Class)
+ * - snake_case: student_name (unpopular Usage)
+ * - kebab-case: menu-item (Css class name)
  */
