cccc83dbfbe8ce0e586ae4a1d284839630bf8222
Yen Ka <yen@pulsedigital.co.uk>
2025-04-06 19:35:13 +0700

    [Javascript]: Exercises overview

---
+++ javascript/exercises-overview/practice-array-subarr.js
@@ -0,0 +1,11 @@
+/**
+ * : Tìm các mảng con có chứa số dương chẳn liên tiếp
+  Viết hàm findAllPositiveEvenSubArr(numberList) để tìm ra tất cả các mảng con chỉ chứa các số dương
+  chẳn.
+  findAllPositiveEvenSubArr([1, 2, 4, 3, 5, 10, 20]);
+  // should return
+  // [
+  // [2, 4],
+  // [10, 20]
+  // ]
+ */
